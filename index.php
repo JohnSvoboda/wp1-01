3d5c8cd77c39dfafd0407a3cb6068545693dc8d0
JanSvoboda <honziceksv@seznam.cz>
2018-03-08 15:00:24 +0100

    přidání index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <?php
+  $age = "16" ;
+
+  $firstname = "Honza" ;
+
+  $lastname = "Svoboda" ;
+
+  $school = " ZŠ Svítkov" ;
+
+
+
+
+
+   ?>
+  <p>Věk: <?php echo "$age"; ?></p>
+  <p>Jméno <?php echo "$firstname"; ?></p>
+  <p>Příjmení <?php echo "$lastname"; ?></p>
+  <p>škola <?php echo "$school"; ?></p>
+  <p>Ahoj já jsem <?php echo "$firstname"; ?>  <?php echo "$lastname"; ?> je mi <?php echo "$age"; ?> let a chodil jsem na <?php echo "$school"; ?></p>
+  </body>
+
+</html>
