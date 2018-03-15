bed2672f8ab18bdf0435fb4c524c21293dbb386b
JanSvoboda <honziceksv@seznam.cz>
2018-03-15 13:55:57 +0100

    dobrý den

---
+++ pR2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <?php $promenna= 5;
+  $a= 1;
+  $b= 2;
+  $soucet=$a+$b ;
+
+  $soucin=$a*$b ;
+
+  $podil=$a-$b ;
+
+  $rozdil=$a/$b ;
+  ?>
+
+  <?php
+  echo "Součet: $a + $b = $soucet"; ?> <br>
+  <?php
+  echo "Součin: $a * $b = $soucin"; ?> <br>
+  <?php
+  echo "podíl = $a - $b = $podil"; ?> <br>
+  <?php
+  echo "rozdíl = $a / $b = $rozdil"; ?> <br>
+
+
+
+
+  </body>
+</html>
